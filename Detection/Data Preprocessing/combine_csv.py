fc3592acb3c70e6445426ed45ab258682aac6197
Karnbongkot Boonriong <2714363b@student.gla.ac.uk>
2023-03-03 09:04:03 +0000

    Updare scripts

--- Detection/Data Preprocessing/combine_csv.py
+++ Detection/Data Preprocessing/combine_csv.py
@@ -3,8 +3,11 @@ import pandas as pd
 import numpy as np
 
 pd.set_option('display.max_rows', None)
-files = glob.glob("training/os/*.csv")
-combined_csv = pd.concat([pd.read_csv(f) for f in files ])
-combined_csv = combined_csv.sort_values(['date', 'time'], ascending=[True, True])
-print(combined_csv)
-combined_csv.to_csv( "os_training_time.csv", index=True, encoding='utf-8-sig')
+# files = ['arp','combination','injection','readbomb','recon','synflood']
+files = ['malware']
+for fname in files:
+    file = glob.glob("training/"+fname+"/*.csv")
+    combined_csv = pd.concat([pd.read_csv(f) for f in file ])
+    combined_csv = combined_csv.sort_values(['date', 'time'], ascending=[True, True])
+    print(combined_csv)
+    combined_csv.to_csv( "testsets/"+fname+"2.csv", index=False, encoding='utf-8-sig')
---
+++ Detection/Data Preprocessing/filter_for_entropy.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+import glob
+
+# files = ['arp','combination','injection','malware','readbomb','recon','synflood']
+files = ['malware2']
+for file in files:
+    data = pd.read_csv('testsets/'+file+'.csv')
+    date = data['date']
+    data = data.drop(['date','ip.dsfield','ip.frag_offset','ip.version','eth.type','ip.hdr_len','raw'], axis = 1)
+    col = data.columns
+    data = data.to_numpy()
+    for i in range(data.shape[0]):
+        data[i,0] = str(date[i])+str(data[i,0])
+    timestamps = sorted(np.unique(data[:,0]))
+    for i in range(len(data[:,0])):
+        print(i)
+        data[i,0] = timestamps.index(data[i,0])
+    data = pd.DataFrame(data)
+    data.columns = col
+    data = data.sort_values(['time'], ascending=[True])
+    data.to_csv('testsets/raw_'+file+'.csv', index=False)
